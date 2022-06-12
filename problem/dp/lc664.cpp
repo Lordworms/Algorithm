1d203066b378465c1065078c696a52c292cf256e
SM2792 <1683173725@qq.com>
2022-06-12 23:54:31 +0800

    新增周赛

---
+++ problem/dp/lc664.cpp
@@ -0,0 +1,25 @@
+/*
+    题面：有台奇怪的打印机有以下两个特殊要求：
+    打印机每次只能打印由 同一个字符 组成的序列。
+    每次可以在从起始到结束的任意位置打印新字符，并且会覆盖掉原来已有的字符。
+    给你一个字符串 s ，你的任务是计算这个打印机打印它需要的最少打印次数。
+    例如对于字符串"aba"先会打印aaa，然后打印b，一共打印两次
+    题解：
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int strangePrinter(string s) {
+        int n=s.size();
+        vector<int>pre,next;
+    }
+};
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  return 0;
+}
