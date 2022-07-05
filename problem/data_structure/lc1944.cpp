d1cb9204823095b435b96c186d51889f6513c3d3
向彦鑫 <xiangyanxin@xiangyaxindeMBP.domain.name>
2022-07-05 22:31:16 +0800

    stupid me

--- problem/data_structure/lc1944.cpp
+++ problem/data_structure/lc1944.cpp
@@ -4,7 +4,7 @@
 一个人能 看到 他右边另一个人的条件是这两人之间的所有人都比他们两人 矮 。更正式的，第 i 个人能看到第 j 个人的条件是 i < j 且 min(heights[i], heights[j]) > max(heights[i+1], heights[i+2], ..., heights[j-1]) 。
 
 请你返回一个长度为 n 的数组 answer ，其中 answer[i] 是第 i 个人在他右侧队列中能 看到 的 人数 
-题解：
+题解：单调栈，因为是向右看，所以从后向前遍历即可
 */
 #include <bits/stdc++.h>
 using namespace std;
