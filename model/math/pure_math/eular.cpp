0d7966faf83e2d62fb24f4516f87ae0e179014d5
向彦鑫 <xiangyanxin@xiangyaxindeMBP.domain.name>
2022-07-12 08:48:42 +0800

    math

---
+++ model/math/pure_math/eular.cpp
@@ -0,0 +1,18 @@
+/*
+    欧拉函数：什么是欧拉函数呢？我们记φ(n)为小于x且和x互质的数的数量，那么φ(n)就是欧拉函数
+    φ(n)=n*(1-1/p1)(1-1/p2)(1-1/p3)*(1-1/p4)……(1-1/pn)
+    
+*/
+#include <bits/stdc++.h>
+using namespace std;
+using ll=unsigned long long;
+ll eular(int x)
+{
+    ll ans=x;
+}
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  return 0;
+}
---
+++ problem/math/big_num/cf_beta_17.cpp
@@ -0,0 +1,19 @@
+/*
+Nick is attracted by everything unconventional. He doesn't like decimal number system any more, 
+and he decided to study other number systems. A number system with base b caught his attention. 
+Before he starts studying it, he wants to write in his notepad all the numbers of length n without 
+leading zeros in this number system. Each page in Nick's notepad has enough space for c numbers exactly. 
+Nick writes every suitable number only once, starting with the first clean page and leaving no clean spaces. 
+Nick never writes number 0 as he has unpleasant memories about zero divide.
+Would you help Nick find out how many numbers will be written on the last page.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  
+  return 0;
+}
