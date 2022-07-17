d6b72c8eb99ce1a5525b422b8ee3f42dc2379106
向彦鑫 <xiangyanxin@xiangyaxindeMBP.domain.name>
2022-07-17 21:58:57 +0800

    1

--- test.cpp
+++ test.cpp
@@ -2,40 +2,30 @@
 using namespace std;
 class Solution {
 public:
-    int minOperations(vector<int>& nums, vector<int>& numsDivide) {
-        map<int,int>mp;
-        for(int num:nums)mp[num]++;
-        sort(numsDivide.begin(),numsDivide.end());
-        int nu=numsDivide[0];
-        if(numsDivide.size()>1){
-            nu=gcd(numsDivide[0],numsDivide[1]);
-            for(int i=2;i<numsDivide.size();++i)nu=__gcd(__gcd(numsDivide[i],numsDivide[0]),nu);
-        }
-        unordered_set<int>st;
-        for(int i=1;i<=nu/2;++i)
+    int maximumSwap(int num) {
+        string s=to_string(num);
+        string tmp=s;
+        string ans=s;
+        for(int i=0;i<s.size()-1;++i)
         {
-            if(nu%i==0)st.insert(i);
-        }
-        st.insert(nu);
-        int ans=0;
-        for(auto& it:mp)
-        {
-            if(st.count(it.first))
+            for(int j=i+1;j<s.size();++j)
             {
-                return ans;
+                swap(tmp[i],tmp[j]);
+                if(tmp>ans)
+                {
+                    ans=tmp;
+                }
+                swap(tmp[j],tmp[i]);
             }
-            ans+=it.second;
         }
-        return -1;
+        return atoi(ans.c_str());
     }
 };
 int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  vector<int>nums={3,2,6,2,35,5,35,2,5,8,7,3,4};
-  vector<int>nums1={105,70,70,175,105,105,105};
   Solution sol;
-  sol.minOperations(nums,nums1);
+  sol.maximumSwap(2736);
   return 0;
 }
