5cd2b1811abcadc6bb289b853f357c84473daacd
向彦鑫 <xiangyanxin@xiangyanxindeMacBook-Pro.local>
2022-07-18 23:36:06 +0800

    1

---
+++ model/math/pure_math/light_reflect.cpp
@@ -0,0 +1,15 @@
+/*
+光线反射问题，如果和四个顶点有交点，那么将这个矩形视为无限大平面，对应的平面也应该和原本轨迹线有交点
+*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+};
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  Solution sol;
+  return 0;
+}
