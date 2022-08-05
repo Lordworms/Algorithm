5eb2915e5ca87ccf8f8a1a541deaee467087d1ca
SM2792 <1683173725@qq.com>
2022-08-05 11:48:53 +0800

    1

--- model/math/pure_math/mubios.cpp
+++ model/math/pure_math/mubios.cpp
@@ -1,34 +1,46 @@
-/*
-莫比乌斯反演板子
-*/
 #include <bits/stdc++.h>
 using namespace std;
-const int MAXN=1e5+10;
-//linear solution
+#define int long long
+const int MOD=1e9+7;
+const int MAXN=1e6+10,LIM=1e6;
+int x,y;
 int mu[MAXN],prim[MAXN];
 bool flag[MAXN];
-void getMu(int n){
-  mu[1]=1;
-  int cnt=0;
-  memset(flag,false,sizeof(flag));
-  for(int i=2;i<=n;++i)
-  {
-    if(!flag[i])prim[cnt++]=i,mu[i]=-1;
-    for(int j=0;j<cnt&&i*prim[j]<=n;++j)
+int quick_pow(int base,int exp){int ans=1;while(exp){if(exp&1)ans=ans*base%MOD;exp>>=1,base=base*base%MOD;}return ans;}
+void init(int n)
+{
+    int cnt=0;
+    mu[1]=1;
+    for(int i=2;i<=n;++i)
     {
-        flag[i*prim[j]]=true;
-        if(i%prim[j]==0)
+        if(!flag[i])prim[cnt++]=i,mu[i]=-1;
+        for(int j=0;i*prim[j]<=n;++j)
         {
-          mu[i*prim[j]]=0;
-          break;
+            flag[i*prim[j]]=true;
+            if(i%prim[j]==0)
+            {
+                mu[i*prim[j]]=0;
+                break;
+            }
+            mu[i*prim[j]]=-mu[i];
         }
-        mu[i*prim[j]]=-mu[i];
+    }
+}
+int Mu(int x)//线性求
+{
+  int ans=1;
+  for(int j=0;prim[j]*prim[j]<=x;++j)
+  {
+    if(x%prim[j]==0)
+    {
+        int c=0;
+        while(x%prim[j]==0)x/=prim[j],++c;
+        if(c>1){
+          return 0;
+        }
+      ans=-ans;
     }
   }
-}
-int main()
-{
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  return 0;
+  if(x!=1)ans=-ans;
+  return ans;
 }
