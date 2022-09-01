afa3f8541db0f936cbbc3f5879095b757483eebe
向彦鑫 <xiangyanxin@xiangyanxindeMacBook-Pro.local>
2022-09-01 20:05:35 +0800

    hk_airport_remninance

---
+++ problem/dp/cf_1527E.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file cf_1527E.cpp
+ * @author yanxinxiang
+ * @brief 
+ * @version 0.1
+ * @date 2022-09-01
+ * 
+ * @copyright Copyright (c) 2022
+ * dp[i][j]表示前i项分成j块的值，那么我们现在来思考转移方程的问题
+ * dp[i][j]=dp[i-1][j-1]+
+ */
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  return 0;
+}
