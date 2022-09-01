8b8647805c528222f837d96f78a66c65ba7e7cba
向彦鑫 <xiangyanxin@xiangyanxindeMacBook-Pro.local>
2022-09-01 20:05:35 +0800

    hk_airport_remninance

--- model/data_structure/segment_tree_related/segment_tree.cpp
+++ model/data_structure/segment_tree_related/segment_tree.cpp
@@ -1,46 +1,79 @@
+/*
+    线段树可以在O(logn)的时间复杂度内实现单点修改、区间修改、区间查询（区间求和，求区间最大值，求区间最小值）等操作。
+    segment_tree终极版子，可以适用于基本所有情况
+
+*/
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn=1e5+100;
-int arr[maxn];
-struct node
-{
-    int l,r,v;
-}tree[4*maxn];
-void build(int c,int l,int r)
-{
-    tree[c].l=l,tree[c].r=r;
-    if(l==r){tree[c].v=arr[l];return;}
-    int m=(l+r)>>1;
-    build(c<<1,l,m),build(c<<1|1,m+1,r);
-    tree[c].v=tree[c<<1].v+tree[c<<1|1].v;//区间求和
-    //tree[c].v=max(tree[c<<1],tree[c<<1|1]);//区间最大值
-}
-void update(int c,int x,int v)
-{
-    if(tree[c].l==tree[c].r)
+template<typename T>
+class segmentTree{
+    vector<T>tree,lazy;
+    vector<T>*arr;
+    int n,root,n4,end;
+    //build(0,n,1)
+    void build(int l,int r,int c)
     {
-        tree[c].v+=v;//区间加和
-        //tree[c].v=v;//区间更新
-        return;
+        if(s==t)
+        {
+            tree[c]=(*arr)[s];
+            return;
+        }
+        int m=(l+r)>>1;
+        build(l,m,c<<1);
+        build(m+1,r,c<<1|1);
+        tree[c]=tree[c<<1]+tree[c<<1|1];
     }
-    int m=(tree[c].l+tree[c].r)>>1;
-    if(x<=m)update(c<<1,x,v);
-    else update(c<<1|1,x,v);
-    tree[c].v=tree[c<<1].v+tree[c<<1|1].v;
-}
-//调用方式query(c,start,end)
-int query(int c,int l,int r)
+    void push_down(int cl,int cr,int c)//传递懒标记
+    {
+        int cm=(cl+cr)>>1;    
+        if(lazy[c]&&cl!=cr)
+        {
+            lazy[c<<1]+=lazy[c];
+            lazy[c<<1|1]+=lazy[c];
+            tree[c<<1]+=(cm-cl+1)*lazy[c];
+            tree[c<<1|1]+=(cr-cm)*lazy[c];
+            lazy[c]=0;
+        }
+    }
+    void push_up(int c)//根据不同的需求进行更改
+    {
+        tree[c]=tree[c<<1]+tree[c<<1|1];
+    }
+    //求l-r的区间和
+    T range_sum(int l,int r,int cl,int cr,int c)
+    {
+        if(l>cr||r<cl)return 0;//没有，直接干掉
+        if(l<=cl&&r>=cr)
+        {
+            return tree[c];
+        }
+        int cm=(cl+cr)>>1;
+        T sum=0;
+        push_down(cl,cr,c);
+        sum+=range_sum(l,r,cl,cm,c<<1);
+        sum+=range_sum(l,r,cm+1,cr,c<<1|1);
+        return sum;
+    }
+    //区间加和
+    T range_add(int l,int r,int cl,int cr,int c,T val)
+    {
+        if(l>cr||r<cl)return 0;//没有，直接干掉
+        if(l<=cl&&r>=cr)
+        {
+            lazy[c]+=val;
+            tree[c]+=(cr-cl+1)*lazy[c];
+            return;
+        }
+        int cm=(cr+cl)>>1;
+        push_down(cl,cr,c);
+        range_add(l,r,cl,cm,c<<1,val);
+        range_add(l,r,cm+1,cr,c<<1|1,val);
+        push_up(c);
+    }
+};
+int main()
 {
-    if(l<=tree[c].v&&r>=tree[c].r)return tree[c].v;
-    int m=(tree[c].l+tree[c].r)>>1;
-    int sum=0;
-    if(l<=m)sum+=query(c<<1,l,r);
-    if(r>m)sum+=query(c<<1|1,l,r);
-    return sum;
-}
-int main(){
-   ios::sync_with_stdio(false);
-   cin.tie(nullptr);
-
-   return 0;
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  return 0;
 }
