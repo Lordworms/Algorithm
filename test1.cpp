3f1d6675f9fbef67a3908848b0bca04b52070b3a
向彦鑫 <xiangyanxin@dhcp-10-106-25-114.wireless.northwestern.private>
2022-09-16 14:32:02 -0500

    1

--- test1.cpp
+++ test1.cpp
@@ -1,47 +1,84 @@
 #include <bits/stdc++.h>
+#define pii pair<int, int>
 using namespace std;
-vector<bool>vis;
-vector<vector<int>>newG;
-bool ans=false;
-void dfs(int now,int fa)
-{
-    vis[now]=true;
-    for(int nxt:newG[now])
-    {
-       if(vis[nxt]==true&&nxt!=fa){ans=false;return;}
-       dfs(nxt,now);
+const int N = 105, C = 1e8;
+
+struct piii {
+    int d, l, f;
+    piii (int _d = 0, int _l = 0, int _f = 0) {
+        d = _d, l = _l, f = _f; 
+    }
+    bool operator < (const piii &b) const {
+        if (d == b.d && f == b.f) return l < b.l;
+        if (f == b.f) return d > b.d;
+        return f < b.f;
+    }
+    bool operator == (const piii &b) const {
+        return d == b.d && l == b.l && f == b.f;
+    }
+};
+int n, m, mc, a[N], w[N], f[N][N], c;
+int maxd, head, tail;
+map<pii, bool> mp;
+piii q[N<<3], t;
+pii v;
+
+void bfs() {
+    t = piii(1, 0, 1);
+    q[head = tail = 1] = t;
+    mp[pii(0, 1)] = 1;
+    while (head <= tail) {
+        t = q[head++];
+        if (t.d >= maxd) continue;
+        if (mp[v = pii(t.l+1, t.f)] == 0) {
+            mp[v] = 1;
+            q[++tail] = piii(t.d+1, t.l+1, t.f);
+        }
+        if (t.l > 1 && 1ll*t.f*t.l <= C && mp[v = pii(t.l, t.f*t.l)] == 0) {
+            mp[v] = 1;
+            q[++tail] = piii(t.d+1, t.l, t.f*t.l);
+        }
     }
 }
-bool solution(int n, vector<vector<int>> g, vector<int> v) {
-   unordered_set<int>st;
-   for(int p:v)st.insert(p);
-   newG.resize(n+1);
-   vis.resize(n+1,false);
-   vector<int>in(n+1,0);
-   for(auto edge:g)
-   {
-       if(st.count(edge[0])&&!st.count(edge[1])){newG[v[0]].push_back(edge[1]);in[edge[1]]++;}
-       else if(st.count(edge[1])&&!st.count(edge[0])){newG[edge[0]].push_back(v[0]);in[v[0]]++;}
-       else if(st.count(edge[1]&&st.count(edge[0])))continue;
-       else {newG[edge[0]].push_back(edge[1]);in[edge[1]]++;}
-   }
-   int now;
-   for(int i=1;i<=n;++i)
-   {
-       if((st.count(i)&&in[v[0]])||in[i])continue;
-       now=st.count(i)?v[0]:i;
-       break;
-   }
-   dfs(now,0);
-   return ans?false:true;
-}
-
-int main()
-{
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  vector<vector<int>>G={{1, 2}, {2, 3}, {2, 4}, {2, 5}, {4, 6}, {5, 6}};
-  solution(6,G,{1,2,4});
-  return 0;
-
+int main() {
+    scanf("%d%d%d", &n, &m, &mc);
+    for (int i = 1; i <= n; i++) scanf("%d", &a[i]);
+    for (int i = 1; i <= n; i++) scanf("%d", &w[i]);
+    memset(f, -1, sizeof(f));
+    f[0][0] = mc;
+    for (int i = 1; i <= n; i++)
+        for (int j = 0; j <= i; j++) {
+            if (f[i-1][j]-a[i] >= 0)
+                f[i][j] = max(min(f[i-1][j]-a[i]+w[i], mc), f[i][j]);
+            if (j && f[i-1][j-1]-a[i] >= 0)
+                f[i][j] = max(f[i-1][j-1]-a[i], f[i][j]);
+            if(f[i][j]>=0)maxd=max(maxd,j);
+        }
+    /*
+    for (int i = 1; i <= n; i++)
+        for (int j = 0; j <= i; j++)
+            if (f[i][j] >= 0) maxd = max(maxd, j);
+    */
+    bfs();
+    sort(q+1, q+tail+1);
+    for (int i = 1; i <= m; i++) {
+        scanf("%d", &c); int r;
+        if (c <= maxd) {
+            puts("1"); continue;    
+        }
+        for (int i = 1; i <= tail; i++)
+            if (q[i].f <= c && q[i].f+maxd-q[i].d >= c) {
+                puts("1"); goto qwq;
+            }
+        r = tail;
+        for (int l = 1; l <= r; l++) {
+            while (l <= r && q[l].f+q[r].f > c) --r;
+            if (l <= r && q[l].f+q[r].f+maxd-q[l].d-q[r].d >= c) {
+                puts("1"); goto qwq;
+            }
+        }
+        puts("0");
+        qwq: 1;
+    }
+    return 0;
 }
