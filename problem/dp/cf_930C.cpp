c4be27fb0aec8455c8c71c543bee0d17f0dad111
Lordworms <yanxinxiang@tencent.com>
2023-06-25 13:05:55 +0800

    daily commit

--- problem/dp/cf_930C.cpp
+++ problem/dp/cf_930C.cpp
@@ -1,22 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll=long long;
-const int MOD=1e9+7;
-void solve(){
-  
-  return;
-}
 int main(){
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  #ifdef LOCAL
-  freopen("/Users/xiangyanxin/code/Algorithom/in.txt","r",stdin);
-  freopen("/Users/xiangyanxin/code/Algorithom/out.txt","w",stdout);
-  #endif
-  int T;
-  cin>>T;
-  while(T--){
-    solve();
+  int n,m;
+  cin>>n>>m;
+  vector<int>dif(m+2);
+  for(int i=0;i<n;++i){
+    int l,r;
+    cin>>l>>r;
+    ++dif[l],--dif[r+1];
   }
-  return 0;
+  for(int i=1;i<=m;++i){
+    dif[i]+=dif[i-1];
+  }
+  vector<int>seq(m+1);
+  int len=0;
+  vector<int>dp1(m+1),dp2(dp1);
+  for(int i=1;i<=m;++i){
+    if(dif[i]>=seq[len]){
+      seq[++len]=dif[i];
+    }else{
+      int pos=upper_bound(seq.begin()+1,seq.begin()+len+1,dif[i])-seq.begin();
+      seq[pos]=dif[i];
+    }
+    dp1[i]=len;
+  }
+  len=0;
+  fill(seq.begin(),seq.end(),0);
+  for(int i=m;i>=1;--i){
+    if(dif[i]>=seq[len]){
+      seq[++len]=dif[i];
+    }else{
+      int pos=upper_bound(seq.begin()+1,seq.begin()+len+1,dif[i])-seq.begin();
+      seq[pos]=dif[i];
+    }
+    dp2[i]=len;
+  }
+  int ans=0;
+  for(int i=1;i<=m;++i){
+    ans=max(ans,dp1[i]+dp2[i]-1);
+  }
+  cout<<ans<<'\n';
 }
