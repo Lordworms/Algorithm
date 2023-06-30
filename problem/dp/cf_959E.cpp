cd59af218bd4c767e21f272e617f88a5375d2f98
Lordworms <yanxinxiang@tencent.com>
2023-06-30 15:29:44 +0800

    add comment

--- problem/dp/cf_959E.cpp
+++ problem/dp/cf_959E.cpp
@@ -7,11 +7,16 @@ int main(){
     /*
         f(x)=f(x-1)+()
     */
+    /*
+        how to calculate the prefix sum of lowbit within log(n) time?
+        well, we could think about such a situation that for every odd number within the range
+        we must only add 1 to the ans, and then we right shift for 1 bit and then we found other 'odd' number in 'even' number
+    */
     ll ans=0,base=1;
     while(n>1){
-        ans+=base*(n>>1);
-        n-=(n>>1);
-        base<<=1;
+       ans+=base*(n>>1);
+       n-=(n>>1);
+       base<<=1;
     }
     cout<<ans<<'\n';
     return 0;
