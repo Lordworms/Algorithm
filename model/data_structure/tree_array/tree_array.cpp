03b463ce2ff0491b8435ae880903cbc3ae4c74e2
Lordworms <yanxinxiang@tencent.com>
2023-08-23 11:14:27 +0800

    daily commit

--- model/data_structure/tree_array/tree_array.cpp
+++ model/data_structure/tree_array/tree_array.cpp
@@ -6,97 +6,93 @@
 */
 #include <bits/stdc++.h>
 using namespace std;
-const int MAXN=1e5+5;
-struct fenwick
-{
-	int PartialSum[MAXN];
-	fenwick()
-	{
-		for(int i = 0; i < MAXN; i++)PartialSum[i] = 0;
-	}
-	inline void add(int index, bool increase)
-	{
-		while(index < MAXN)
-		{
-			PartialSum[index] += (increase? 1 : -1);
-			index += index&-index;
-		}
-	}
-	inline int get(int index)
-	{
-		int sum = 0;
-		while(index)
-		{
-			sum += PartialSum[index];
-			index -= index&-index;
-		}
-		return sum;
-	}
+const int MAXN = 1e5 + 5;
+struct fenwick {
+  ll PartialSum[MAXN];
+  fenwick() {
+    for (int i = 0; i < MAXN; i++) PartialSum[i] = 0;
+  }
+  inline void add(ll index, ll increase) {
+    while (index < MAXN) {
+      PartialSum[index] += increase;
+      index += index & -index;
+    }
+  }
+  inline ll get(int index) {
+    ll sum = 0;
+    while (index) {
+      sum += PartialSum[index];
+      index -= index & -index;
+    }
+    return sum;
+  }
 };
 class Solution {
-public:
-    vector<int>discrete(vector<int>nums)
-    {
-        vector<int>b(nums);
-        sort(b.begin(),b.end());
-        b.erase(unique(b.begin(),b.end()),b.end());//unique rearrange the data and return the end position, then erase just delete them between b.end()
-        for(int i=0;i<nums.size();++i)
-            nums[i]=lower_bound(b.begin(),b.end(),nums[i])-b.begin()+1;
-        return nums;
-    }
-    int reversePairs(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>tree(n+1,0);
-        auto lowbit=[&](int x){return x&-x;};
-        auto add=[&](int x,int v){while(x<=n){tree[x]+=v,x+=lowbit(x);}};
-        auto sum=[&](int x){int ans=0;while(x){ans+=tree[x],x-=lowbit(x);}return ans;};
-        nums=discrete(nums);
-        int ans=0;
-        for(int i=0;i<n;++i)
-        {
-            ans+=sum(n)-sum(nums[i]);//计算逆序对
-            add(nums[i],1);
-        }
-        return ans;
-    }
-};
-int c[MAXN],a[MAXN];
-int n;
-//获得x末尾有多少个0
-int lowbit(int x)
-{
-    return x&-x;
-}
-int getSum(int x)
-{
-    int ans=0;
-    while(x)
-    {
-        ans+=c[x];
-        x-=lowbit(x);
+ public:
+  vector<int> discrete(vector<int> nums) {
+    vector<int> b(nums);
+    sort(b.begin(), b.end());
+    b.erase(unique(b.begin(), b.end()),
+            b.end());  // unique rearrange the data and return the end position,
+                       // then erase just delete them between b.end()
+    for (int i = 0; i < nums.size(); ++i)
+      nums[i] = lower_bound(b.begin(), b.end(), nums[i]) - b.begin() + 1;
+    return nums;
+  }
+  int reversePairs(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> tree(n + 1, 0);
+    auto lowbit = [&](int x) { return x & -x; };
+    auto add = [&](int x, int v) {
+      while (x <= n) {
+        tree[x] += v, x += lowbit(x);
+      }
+    };
+    auto sum = [&](int x) {
+      int ans = 0;
+      while (x) {
+        ans += tree[x], x -= lowbit(x);
+      }
+      return ans;
+    };
+    nums = discrete(nums);
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+      ans += sum(n) - sum(nums[i]);  // 计算逆序对
+      add(nums[i], 1);
     }
     return ans;
+  }
+};
+int c[MAXN], a[MAXN];
+int n;
+// 获得x末尾有多少个0
+int lowbit(int x) { return x & -x; }
+int getSum(int x) {
+  int ans = 0;
+  while (x) {
+    ans += c[x];
+    x -= lowbit(x);
+  }
+  return ans;
 }
-void update(int x,int value)
-{
-    a[x]+=value;
-    while(x<=n)
-    {
-        c[x]+=value;
-        x+=lowbit(x);
-    }
+void update(int x, int value) {
+  a[x] += value;
+  while (x <= n) {
+    c[x] += value;
+    x += lowbit(x);
+  }
 }
 
-int main(){
-   ios::sync_with_stdio(false);
-   cin.tie(nullptr);
-    int x,v,T;
-    cin>>n;
-    for(int i=1;i<=n;++i)
-    {
-        cin>>a[i];
-        update(i,a[i]);
-    }
-    
-   return 0;
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int x, v, T;
+  cin >> n;
+  for (int i = 1; i <= n; ++i) {
+    cin >> a[i];
+    update(i, a[i]);
+  }
+
+  return 0;
 }
