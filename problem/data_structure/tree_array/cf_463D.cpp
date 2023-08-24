c8c96d28ceef5205ccb0c8ca6aa4092fb976019d
Lordworms <yanxinxiang@tencent.com>
2023-08-24 10:51:40 +0800

    daily commit

--- problem/data_structure/tree_array/cf_463D.cpp
+++ problem/data_structure/tree_array/cf_463D.cpp
@@ -3,22 +3,22 @@ using namespace std;
 using ll = long long;
 const int MOD = 1e9 + 7;
 void solve() {
-  int n, k;
-  cin >> n >> k;
-  vector<vector<int>> pos(k + 1, vector<int>(n + 1)), a(pos);
-  for (int i = 1; i <= k; ++i) {
+  int n, K;
+  cin >> n >> K;
+  vector<vector<int>> pos(K + 1, vector<int>(n + 1)), a(pos);
+  for (int i = 1; i <= K; ++i) {
     for (int j = 1; j <= n; ++j) {
       cin >> a[i][j];
       pos[i][a[i][j]] = j;
     }
-  }
+  } 
   vector<int> dp(n + 1, 1);
-  int ans = 0;
-  for (int i = 1; i <= n; ++i) {
+  int ans = 1;
+  for (int i = 2; i <= n; ++i) {
     for (int j = 1; j < i; ++j) {
       bool flag = true;
-      for (int kk = 2; kk <= k; ++kk) {
-        if (pos[kk][a[1][i]] <= pos[kk][a[1][j]]) {
+      for (int k = 2; k <= K; ++k) {
+        if (pos[k][a[1][j]] > pos[k][a[1][i]]) {
           flag = false;
           break;
         }
@@ -30,14 +30,14 @@ void solve() {
     ans = max(ans, dp[i]);
   }
   cout << ans << '\n';
-  return;
+  return ;
 }
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-#ifdef LOCAL
-  freopen("/Users/xiangyanxin/code/Algorithom/in.txt", "r", stdin);
-  freopen("/Users/xiangyanxin/code/Algorithom/out.txt", "w", stdout);
+#ifdef DEBUG
+  freopen("/Users/yanxinxiang/code/Algorithm/in", "r", stdin);
+  freopen("/Users/yanxinxiang/code/Algorithm/out", "w", stdout);
 #endif
   int T = 1;
   while (T--) {
