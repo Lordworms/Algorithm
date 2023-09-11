cc6496e630699d5daa43aa63672911c8796861f9
Lordworms <yanxinxiang@tencent.com>
2023-09-11 17:32:34 +0800

    daily commit

--- problem/dp/cf_1635D.cpp
+++ problem/dp/cf_1635D.cpp
@@ -5,19 +5,49 @@ const int MOD = 1e9 + 7;
 void solve() {
   int n, p;
   cin >> n >> p;
-  vector<ll> f(p + 1), sum(p + 1);
   vector<int> a(n + 1);
+  vector<int> b;
   unordered_set<int> st;
+  function<bool(int)> check_encounter = [&] (int x) {
+    if (!x) {
+      return false;
+    }
+    if (st.count(x)) {
+      return true;
+    }
+    if (x % 4 == 0) {
+      return check_encounter(x >> 2);
+    }
+    if (x & 1) {
+      return check_encounter((x - 1) >> 1);
+    }
+    return false;
+  };
   for (int i = 1; i <= n; ++i) {
     cin >> a[i];
   } 
   sort(a.begin() + 1, a.end());
-  auto check = [&](int x) {
-    
-  };
-  for (int &x : a) {
-
+  for (int i = 1; i <= n; ++i) {
+    if (!check_encounter(a[i])) {
+      b.push_back(a[i]);
+      st.insert(a[i]);
+    }
+  } 
+  vector<ll> f(2e5 + 10);
+  for (auto& i : b) {
+    int cnt = 0, x = i;
+    while(x) {
+     ++cnt;
+     x >>= 1;
+    }
+    f[cnt]++;
   }
+  ll ans = f[1];
+  for (int i = 2; i <= p; ++i) {
+    f[i] = (f[i] + f[i - 1] + f[i - 2]) % MOD;
+    ans = (ans + f[i]) % MOD;
+  }
+  cout << ans << '\n';
   return ;
 }
 int main() {
--- problem/dp/cf_580D.cpp
+++ problem/dp/cf_580D.cpp
@@ -3,8 +3,42 @@ using namespace std;
 using ll = long long;
 const int MOD = 1e9 + 7;
 void solve() {
-   
-  return ;
+  int n, m, k;
+  cin >> n >> m >> k;
+  vector<int> a(n);
+  vector<vector<ll>> dp(n,
+                        vector<ll>(1 << n));  // the status is sta and ends in i
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+    dp[i][1 << i] = a[i];
+  }
+  int u, v, c;
+  vector<vector<int>> mp(n, vector<int>(n));
+  for (int i = 0; i < k; ++i) {
+    cin >> u >> v >> c;
+    --u, --v;
+    mp[u][v] = c;
+  }
+  for (int sta = 0; sta < (1 << n); ++sta) {
+    for (int i = 0; i < n; ++i) {
+      if (!(sta & (1 << i))) continue;
+      for (int j = 0; j < n; ++j) {
+        if (i == j || !(sta & (1 << j))) continue;
+        dp[i][sta] = max(dp[i][sta], dp[j][sta ^ (1 << i)] + mp[j][i] + a[i]);
+      }
+    }
+  }
+  ll ans = 0;
+  for (int i = 0; i < (1 << n); ++i) {
+    if (__builtin_popcount(i) != m) {
+      continue;
+    }
+    for (int j = 0; j < n; ++j) {
+      ans = max(ans, dp[j][i]);
+    }
+  }
+  cout << ans << '\n';
+  return;
 }
 int main() {
   ios::sync_with_stdio(false);
