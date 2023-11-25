cbe399527fe8b77303c62656c8466a6241dbd3e1
Yanxin Xiang <yanxinxiang0917@outlook.com>
2023-11-25 18:37:16 +0800

    daily commit

--- problem/dp/cf_1806D.cpp
+++ problem/dp/cf_1806D.cpp
@@ -1,9 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-const int MOD = 1e9+7;
+const int MOD = 1e9 + 7;
 void solve() {
-  
+  int n;
+  cin >> n;
+  vector<int> a(n + 1), dp(n + 1);
+  for (int i = 1; i <= n - 1; ++i) cin >> a[i];
+  dp[1] = 1;
+  for (int i = 1; i <= n - 1; ++i) {
+    dp[i + 1] = dp[i] * (i - a[i]) % MOD;
+  }
+  int ans = 0;
+  for (int i = 1; i < n; ++i) {
+    ans = (ans * i + !a[i] * dp[i]) % MOD;
+    cout << ans << " \n" [i == n - 1];
+  }
   return;
 }
 int main() {
@@ -14,6 +26,7 @@ int main() {
   freopen("/Users/xiangyanxin/code/Algorithom/out.txt", "w", stdout);
 #endif
   int T = 1;
+  cin >> T;
   while (T--) {
     solve();
   }
