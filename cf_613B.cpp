e7bf009d2b09eaaf3942a1c1501ca7557f40bc9a
Yanxin Xiang <yanxinxiang0917@outlook.com>
2023-11-29 18:20:41 +0800

    daily commit

--- cf_613B.cpp
+++ cf_613B.cpp
@@ -1,65 +1,67 @@
-#include<cstdio>
-#include<cstring>
-#include<cmath>
-#include<cstdlib>
-#include<vector>
-#include<map>
-#include<queue>
-#include<algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-#define inf 0x3f3f3f3f
-typedef long long LL;
-struct inst {
-	long long  x;
-	long long  id;
+using ll = long long;
+const int MOD = 1e9+7;
+#define int ll
+struct skill {
+  int id, v;
+  bool operator < (const skill &a) const {
+    return v < a.v;
+  }
 };
-inst ax[100100];
-long long  mam[100100];
-pair<long long, pair<long long, long long>>ans;
-long long  cmp(inst a, inst b) {
-	return a.x < b.x;
+void solve() {
+  int n, A, cf, cm, m;
+  cin >> n >> A >> cf >> cm >> m;
+  vector<skill> a(n + 1);
+  for (int i = 1; i <= n; ++i) {
+    a[i].id = i;
+    cin >> a[i].v;
+  }
+  sort(a.begin() + 1, a.end(), [&](skill aa, skill bb) {
+    return aa.v < bb.v;
+  });
+  ll cur = n;
+  vector<int> sum(n + 1);
+  for (int i = 1; i <= n; ++i) {
+    sum[i] = sum[i - 1] + a[i].v;
+  }
+  pair<ll, pair<ll, ll>> ans;
+  for (int i = 0; i <= n; ++i) {
+    int rem = m - (i * A - (sum[n] - sum[n - i]));
+    if (rem < 0) break;
+    // rem 
+    cur = min(cur, 1ll * (n - i));
+    while (cur > 1 && rem < (cur * a[cur].v - sum[cur])) --cur; // to get the maximum minimum
+    rem -= (cur * a[cur].v - sum[cur]);
+    int min_level = min(A, (i == n? A : (a[cur].v + rem / cur)));
+    ans = std::max(ans, {i * cf + min_level * cm, {i, min_level}});
+  }
+  cout << ans.first << '\n';
+  for (int i = 0; i < ans.second.first; ++i) {
+    a[n - i].v = A;
+  }
+  // ans: finalanswer, {changed to max nunmber, minimal level}
+  for (int i = 1; i <= n; ++i) {
+    a[i].v = std::max(a[i].v, ans.second.second);
+  }
+  sort(a.begin() + 1, a.end(), [&](skill a, skill b) {
+    return a.id < b.id;
+  });
+  for (int i = 1; i <= n; ++i) {
+    cout << a[i].v << " \n" [i == n];
+  }
+  return;
 }
-long long  cmp2(inst a, inst b) {
-	return a.id < b.id;
-}
-int  main(void) {
-	long long  n, a, ma, mi, k;
-	scanf("%lld%lld%lld%lld%lld", &n, &a, &ma, &mi, &k);
-	for (long long  i = 1; i <= n; i++) {
-		scanf("%lld", &ax[i].x);
-		ax[i].id = i;
-	}
-	sort(ax+1, ax+1 + n,cmp);
-	mam[0] = 0;
-	for (long long  i = 1; i <= n; i++) {
-		mam[i] = mam[i - 1] +ax[i].x;
-	}
-	long long p = n;
-	for (int i = 0; i <=n; i++) {
-		long long dd = k -i*a+mam[n]-mam[n-i];
-		if (dd < 0) {
-			break;
-		}
-		p = min(p, n-i);
-		while (p>1 && dd < p*ax[p].x-mam[p]) {
-			p--;
-		}
-		dd -= p*ax[p].x - mam[p];
-		long long k = min(a, i == n ? a : (ax[p].x + dd / p));
-		ans = max(ans, { i*ma + k*mi,{i,k} });
-	}
-	printf("%lld\n", ans.first);
-	for (int i = 0; i < ans.second.first; i++) {
-		ax[n-i].x = a;
-	}
-	for (int i = 1; i <= n; i++) {
-		ax[i].x = max(ax[i].x, ans.second.second);
-	}
-	sort(ax+1, ax+1 + n, cmp2);	
-	for (long long  i = 1; i <= n; i++) {
-		printf("%lld ", ax[i].x);
-	}
-	printf("\n");
- 
-	return 0;
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+#ifdef LOCAL
+  freopen("/Users/xiangyanxin/code/Algorithom/in.txt", "r", stdin);
+  freopen("/Users/xiangyanxin/code/Algorithom/out.txt", "w", stdout);
+#endif
+  int T = 1;
+  while (T--) {
+    solve();
+  }
+  return 0;
 }
