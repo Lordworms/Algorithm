f45e89e41acaf401f2c1effacac3f8a10a402b47
Yanxin Xiang <yanxinxiang0917@outlook.com>
2023-12-05 22:13:38 +0800

    daily commit

--- problem/constructive_problem/cf_681D.cpp
+++ problem/constructive_problem/cf_681D.cpp
@@ -3,8 +3,49 @@ using namespace std;
 using ll = long long;
 const int MOD = 1e9+7;
 void solve() {
-  
+  int n, m, u, v;
+  cin >> n >> m;
+  vector<vector<int>> E(n + 1);
+  vector<int> want(n + 1), has(n + 1);
+  bool is_valid = true;
+  for (int i = 1; i <= m; ++i) {
+    cin >> u >> v;
+    E[u].push_back(v);
+    E[v].push_back(u);
+    has[v]++;
+  }
+  for (int i = 1; i <= n; ++i) {
+    cin >> want[i];
+  }
+  vector<int> ans;
+  function<void(int, int)> dfs = [&] (int x, int f) {
+    for (int &v : E[x]) {
+      if (v == f) continue;
+      if (want[x] != want[v] && want[v] != v) {
+        is_valid = false;
+      }
+      dfs(v, x);
+    }
+    if (want[x] == x) {
+      ans.push_back(x);
+    }
+  };
+  for (int i = 1; i <= n; ++i) {
+    if (!has[i]) {
+      dfs(i, 0);
+    }
+  }
+  if (!is_valid) {
+    cout << -1 << '\n';
+  } else {
+    cout << ans.size() << '\n';
+    for (auto &k : ans) {
+      cout << k << '\n';
+    }
+    cout << '\n';
+  }
   return;
+
 }
 int main() {
   ios::sync_with_stdio(false);
