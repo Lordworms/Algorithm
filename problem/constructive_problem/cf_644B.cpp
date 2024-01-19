a494c35345d40a0dd12740b2ce6c2fc02c494a10
Yanxin Xiang <yanxinxiang0917@outlook.com>
2024-01-18 20:19:54 -0600

    daily commit

--- problem/constructive_problem/cf_644B.cpp
+++ problem/constructive_problem/cf_644B.cpp
@@ -1,42 +1,80 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll=long long;
-const int MOD=1e9+7;
-void solve(){
-  int n,b;
-  cin>>n>>b;
-  deque<ll>q;
-  vector<ll>t(n),d(n);
-  for(int i=0;i<n;++i){
-    cin>>t[i]>>d[i];
+using ll = long long;
+const int MOD = 1e9+7;
+#define int ll
+struct Query {
+  int id, t, d, finish;
+};
+void solve() {
+  int n, b;
+  cin >> n >> b;
+  vector<Query> a(n + 1);
+  for (int i = 1; i <= n; ++i) {
+    cin >> a[i].t >> a[i].d;
+    a[i].id = i;
   }
-  for(int i=0;i<n;++i){
-    while(q.size()&&q.front()<=t[i]){
-        q.pop_front();
+  sort(a.begin() + 1, a.end(), [&](Query a, Query b) {
+    return a.t < b.t;
+  });
+  queue<Query*> q;
+  int should_finish = 0;
+  bool is_busy = false;
+  for (int i = 1; i <= n; ++i) {
+    if (!is_busy) {
+      is_busy = true;
+      should_finish = std::max(should_finish, a[i].t) + a[i].d; 
+      a[i].finish = should_finish;
+      continue;
     }
-    if(q.empty()){
-        q.push_back(t[i]+d[i]);
-        cout<<q.front()<<' ';
-    }else{
-        if(q.size()>b){
-            cout<<-1<<" ";
-        }else{
-            cout<<q.back()+d[i]<<' ';
-            q.push_back(q.back()+d[i]);
-        }
+    if (q.size() >= b && a[i].t < should_finish) {
+      a[i].finish = -1;
+      continue;
     }
+    if (a[i].t >= should_finish) {
+      if (q.size() > 0) {
+        is_busy = true;
+        should_finish = std::max(q.front()->t, should_finish) + q.front()->d;
+        q.front()->finish = should_finish;
+        q.pop();
+      } else {
+        is_busy = false;
+      }
+    }
+    if (!is_busy) {
+      is_busy = true;
+      a[i].finish = std::max(should_finish, a[i].t) + a[i].d;
+      should_finish = a[i].finish;
+      continue;
+    }
+    if (q.size() < b) {
+      q.push(&a[i]);
+    }
+  }
+  while (!q.empty()) {
+    auto p = q.front();
+    q.pop();
+    should_finish = max(should_finish, p->t) + p->d;
+    p->finish = should_finish;
+  }
+  vector<int> ans(n + 1);
+  for (int i = 1; i <= n; ++i) {
+    ans[a[i].id] = a[i].finish;
+  }
+  for (int i = 1; i <= n; ++i) {
+    cout << ans[i] << " \n" [i == n];
   }
   return;
 }
-int main(){
+signed main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  #ifdef LOCAL
-  freopen("/Users/xiangyanxin/code/Algorithom/in.txt","r",stdin);
-  freopen("/Users/xiangyanxin/code/Algorithom/out.txt","w",stdout);
-  #endif
-  int T=1;
-  while(T--){
+#ifdef LOCAL
+  freopen("/Users/xiangyanxin/code/Algorithom/in.txt", "r", stdin);
+  freopen("/Users/xiangyanxin/code/Algorithom/out.txt", "w", stdout);
+#endif
+  int T = 1;
+  while (T--) {
     solve();
   }
   return 0;
--- redo.md
+++ redo.md
@@ -12,5 +12,6 @@ I think I have done enough problem so far (2024.1.13) from today I would review 
 |2024.1.14|[cf_1628A](problem/constructive_problem/cf_1628A.cpp)|construtctive problem|
 |2024.1.15|[cf_148E](problem/constructive_problem/cf_148E.cpp)|bags|
 |2024.1.16|[cf_633D](problem/constructive_problem/cf_633D.cpp) [cf_1015E2](problem/constructive_problem/cf_1015E2.cpp)| constructive problem|
-
+|2024.1.17|[cf_1015E2](problem/constructive_problem/cf_1015E2.cpp)|constructive problem|
+|2024.1.18|[cf_644B](problem/constructive_problem/cf_644B.cpp)|constructive problem|
 
