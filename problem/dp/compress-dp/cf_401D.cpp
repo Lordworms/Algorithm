2ae39daaa59d2c0316df064e17bec8d79233f1a0
Yanxin Xiang <yanxinxiang0917@outlook.com>
2024-01-24 21:44:11 -0600

    daily commit

--- problem/dp/compress-dp/cf_401D.cpp
+++ problem/dp/compress-dp/cf_401D.cpp
@@ -1,49 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll=long long;
-const int MOD=1e9+7;
-const int N=19;
-ll dp[1<<N][101];
-void solve(){
-  vector<int>num;
-  ll n,m;
-  cin>>n>>m;
-  memset(dp,-1,sizeof(dp));
-  while(n){
-    num.push_back(n%10);
-    n/=10;
+using ll = long long;
+const int MOD = 1e9+7;
+#define int ll
+const int N = 18;
+int a[N];
+int dp[1 << N][111];
+void solve() {
+  ll n, m, c = 1;
+  cin >> n >> m;
+  map<ll, ll> cnt;
+  int len = 0;
+  while (n) {
+    a[len++] = n % 10;
+    cnt[n % 10]++;
+    c *= cnt[n % 10];
+    n /= 10;
   }
-  int len=num.size();
-  sort(num.begin(),num.end());
-  function<ll(int,int,int)>dfs=[&](int pos,int st,ll mod){
-    if(!pos)return (ll)(mod==0);
-    if(~dp[st][mod])return dp[st][mod];
-    ll res=0;
-    for(int i=0;i<len;++i){
-        if(!((st&(1<<i)))&&(i==0||num[i]!=num[i-1]||(st&(1<<(i-1))))){
-            res+=dfs(pos-1,st|(1<<i),(mod*10+num[i])%m);
+  dp[0][0] = 1;
+  for (ll i = 0; i < (1 << len); ++i) {
+    for (ll j = 0; j < len; ++j) {
+      ll bit = i & (1 << j), pre = i || a[j];
+      if (bit == 0 && pre) {
+        for (ll k = 0; k < m; ++k) {
+          dp[i | (1 << j)][(k * 10 + a[j]) % m] += dp[i][k];
         }
-    }
-    return dp[st][mod]=res;
-  };
-  ll ans=0;
-  for(int i=0;i<len;++i){
-    if(num[i]&&(i==0||num[i]!=num[i-1])){
-        ans+=dfs(len-1,1<<i,num[i]%m);
+      } 
     }
   }
-  cout<<ans<<'\n';
+  cout << dp[(1 << len) - 1][0] / c << '\n';
   return;
 }
-int main(){
+signed main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  #ifdef LOCAL
-  freopen("/Users/xiangyanxin/code/Algorithom/in.txt","r",stdin);
-  freopen("/Users/xiangyanxin/code/Algorithom/out.txt","w",stdout);
-  #endif
-  int T=1;
-  while(T--){
+#ifdef LOCAL
+  freopen("/Users/xiangyanxin/code/Algorithom/in.txt", "r", stdin);
+  freopen("/Users/xiangyanxin/code/Algorithom/out.txt", "w", stdout);
+#endif
+  int T = 1;
+  while (T--) {
     solve();
   }
   return 0;
