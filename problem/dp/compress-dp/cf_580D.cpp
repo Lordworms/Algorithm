a6ff14a29173217493c2b7d4ca912e3ee69cd48f
Yanxin Xiang <yanxinxiang0917@outlook.com>
2024-01-25 22:40:12 -0600

    daily commit

--- problem/dp/compress-dp/cf_580D.cpp
+++ problem/dp/compress-dp/cf_580D.cpp
@@ -1,55 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll=long long;
-const int MOD=1e9+7;
-void solve(){
-  int n,m,k;
-  cin>>n>>m>>k;
-  vector<int>a(n);
-  vector<vector<int>>mp(n+1,vector<int>(n+1));
-  vector dp((1<<n),vector<ll>(n));
-  for(int i=0;i<n;++i){
-    cin>>a[i];
-    dp[1<<i][i]=a[i];
+using ll = long long;
+const int MOD = 1e9 + 7;
+void solve() {
+  int n, m, k;
+  cin >> n >> m >> k;
+  vector<int> a(n);
+  vector<vector<ll>> dp(n,
+                        vector<ll>(1 << n));  // the status is sta and ends in i
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+    dp[i][1 << i] = a[i];
   }
-  for(int i=0;i<k;++i){
-    int u,v,c;
-    cin>>u>>v>>c;
-    --u,--v;
-    mp[u][v]=c;
+  int u, v, c;
+  vector<vector<int>> mp(n, vector<int>(n));
+  for (int i = 0; i < k; ++i) {
+    cin >> u >> v >> c;
+    --u, --v;
+    mp[u][v] = c;
   }
-  for(int status=0;status<(1<<n);++status){
-    for(int i=0;i<n;++i){
-        if(status&(1<<i)){
-            for(int j=0;j<n;++j){
-                if(i!=j&&(status&(1<<j))){
-                    dp[status][i]=max(dp[status][i],dp[status^(1<<i)][j]+a[i]+mp[j][i]);
-                }
-            }
-        }
+  for (int sta = 0; sta < (1 << n); ++sta) {
+    for (int i = 0; i < n; ++i) {
+      if (!(sta & (1 << i))) continue;
+      for (int j = 0; j < n; ++j) {
+        if (i == j || !(sta & (1 << j))) continue;
+        dp[i][sta] = max(dp[i][sta], dp[j][sta ^ (1 << i)] + mp[j][i] + a[i]);
+      }
     }
   }
-  ll ans=0;
-  for(int status=0;status<(1<<n);++status){
-    if(__builtin_popcount(status)!=m)continue;
-    for(int i=0;i<n;++i){
-        if(status&(1<<i)){
-            ans=max(ans,dp[status][i]);
-        }
+  ll ans = 0;
+  for (int i = 0; i < (1 << n); ++i) {
+    if (__builtin_popcount(i) != m) {
+      continue;
+    }
+    for (int j = 0; j < n; ++j) {
+      ans = max(ans, dp[j][i]);
     }
   }
-  cout<<ans<<'\n';
+  cout << ans << '\n';
   return;
 }
-int main(){
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  #ifdef LOCAL
-  freopen("/Users/xiangyanxin/code/Algorithom/in.txt","r",stdin);
-  freopen("/Users/xiangyanxin/code/Algorithom/out.txt","w",stdout);
-  #endif
-  int T=1;
-  while(T--){
+#ifdef DEBUG
+  freopen("/Users/yanxinxiang/code/Algorithm/in", "r", stdin);
+  freopen("/Users/yanxinxiang/code/Algorithm/out", "w", stdout);
+#endif
+  int T = 1;
+  while (T--) {
     solve();
   }
   return 0;
